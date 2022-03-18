6b7793c49b1dd6f47c473aaad8ee1e9ebce13034
toptecshare <eestimorol.ice9208g@gmail.com>
2022-03-17 22:26:06 -0500

    claim testing

--- test/index.ts
+++ test/index.ts
@@ -117,7 +117,92 @@ describe("Token", () => {
       expect(await itemFactory._rewardMapping(0, 0)).to.equal(reward);
     });
 
-    it("setMilkContractAddress, setRarityRolls are reverted with non deployer", async () => {
+    describe("test claim function", () => {
+      beforeEach(async () => {
+        await itemFactory.setMilkContractAddress(milk.address);
+        await itemFactory.setRarityRolls(80, 100, 120, 140, 160, 180);
+        let reward0 = web3.eth.abi.encodeParameters(
+          ["uint256", "uint256", "uint256[]"],
+          [20, 80, [2, 3, 4]]
+        );
+        let reward1 = web3.eth.abi.encodeParameters(
+          ["uint256", "uint256", "uint256[]"],
+          [120, 180, [3, 4, 5]]
+        );
+        let reward2 = web3.eth.abi.encodeParameters(
+          ["uint256", "uint256", "uint256[]"],
+          [120, 280, [4, 5, 6]]
+        );
+        let reward3 = web3.eth.abi.encodeParameters(
+          ["uint256", "uint256", "uint256[]"],
+          [220, 380, [5, 6, 7]]
+        );
+        let reward4 = web3.eth.abi.encodeParameters(
+          ["uint256", "uint256", "uint256[]"],
+          [120, 380, [7, 8, 9]]
+        );
+        let reward5 = web3.eth.abi.encodeParameters(
+          ["uint256", "uint256", "uint256[]"],
+          [20, 480, [3, 7, 9]]
+        );
+        let reward6 = web3.eth.abi.encodeParameters(
+          ["uint256", "uint256", "uint256[]"],
+          [220, 580, [4, 6, 8]]
+        );
+        let reward7 = web3.eth.abi.encodeParameters(
+          ["uint256", "uint256", "uint256[]"],
+          [120, 680, [10, 17, 24]]
+        );
+        let reward8 = web3.eth.abi.encodeParameters(
+          ["uint256", "uint256", "uint256[]"],
+          [220, 880, [11, 22, 44]]
+        );
+        let reward9 = web3.eth.abi.encodeParameters(
+          ["uint256", "uint256", "uint256[]"],
+          [20, 980, [24, 36, 48]]
+        );
+
+        await itemFactory.setReward(0, 0, reward0);
+        await itemFactory.setReward(0, 1, reward1);
+        await itemFactory.setReward(0, 2, reward2);
+        await itemFactory.setReward(0, 3, reward3);
+        await itemFactory.setReward(0, 4, reward4);
+        await itemFactory.setReward(1, 0, reward5);
+        await itemFactory.setReward(2, 1, reward6);
+        await itemFactory.setReward(3, 2, reward7);
+        await itemFactory.setReward(4, 3, reward8);
+        await itemFactory.setReward(5, 4, reward9);
+      });
+
+      it("claim function should work", async () => {
+        await itemFactory.claim(deployer.address, 1111);
+      });
+
+      it("claim function can not be called twice a day", async () => {
+        await itemFactory.claim(deployer.address, 1111);
+        await expect(
+          itemFactory.claim(deployer.address, 1111)
+        ).to.be.revertedWith("Claim once per day");
+      });
+
+      it("claim function can be called after a day", async () => {
+        await itemFactory.claim(deployer.address, 1111);
+        await expect(
+          itemFactory.claim(deployer.address, 1111)
+        ).to.be.revertedWith("Claim once per day");
+      });
+    });
+
+    // it("setReward should work with restriction", async () => {
+    //   let reward = web3.eth.abi.encodeParameters(
+    //     ["uint256", "uint256[]"],
+    //     [20, [2, 3, 4]]
+    //   );
+    //   await itemFactory.setReward(0, 0, reward);
+    //   await expect(itemFactory._rewardMapping(0, 0)).to.be.reverted;
+    // });
+
+    it("setMilkContractAddress, setRarityRolls, setReward are reverted with non deployer", async () => {
       await expect(
         itemFactory.connect(addr1).setMilkContractAddress(milk.address)
       ).to.be.revertedWith(
@@ -129,6 +214,19 @@ describe("Token", () => {
 
       await expect(
         itemFactory.connect(addr1).setRarityRolls(80, 100, 120, 140, 160, 180)
+      ).to.be.revertedWith(
+        `AccessControl: account ${addr1.address.toLowerCase()} is missing role ${ethers.utils.hexZeroPad(
+          "0x00",
+          32
+        )}`
+      );
+
+      let reward = web3.eth.abi.encodeParameters(
+        ["uint256", "uint256", "uint256[]"],
+        [20, 80, [2, 3, 4]]
+      );
+      await expect(
+        itemFactory.connect(addr1).setReward(0, 0, reward)
       ).to.be.revertedWith(
         `AccessControl: account ${addr1.address.toLowerCase()} is missing role ${ethers.utils.hexZeroPad(
           "0x00",
