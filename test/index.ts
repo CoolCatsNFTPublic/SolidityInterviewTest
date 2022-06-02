4f4e2d161ec441ce890aead54b893fca170348a5
AyDeveloper <ayoayobami20@gmail.com>
2022-06-02 09:29:49 +0100

    Test for Milk Contract

--- test/index.ts
+++ test/index.ts
@@ -2,11 +2,10 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable no-unused-vars */
 import { expect } from "chai";
-import { Address } from "cluster";
 import { ethers } from "hardhat";
 import { Milk } from "../typechain-types";
 
-describe("Staking Contract", function () {
+describe("Milk Contract", function () {
   let milkContract: Milk;
   beforeEach( async function() {
     const [,signer] = await ethers.getSigners()
@@ -18,26 +17,80 @@ describe("Staking Contract", function () {
     milkContract.deployed();
   });
 
-  it("withdraw", async function () {
-    const [,signer] = await ethers.getSigners()
+  it("Deposit Milk", async function () {
+    const [,signer, signer2] = await ethers.getSigners()
+    const signer1 = await ethers.getSigner(signer.address);
+    const signer2s = await ethers.getSigner(signer2.address);
+
     const MASTER_ROLE = ethers.utils.id("MASTER_ROLE");
-    console.log(MASTER_ROLE);
+    const DEPOSITOR_ROLE = ethers.utils.id("DEPOSITOR_ROLE");
 
-    // 0x8b8c0776df2c2176edf6f82391c35ea4891146d7a976ee36fd07f1a6fb4ead4c
+    const bigNum = ethers.BigNumber.from("50")
+    const depositData = "0x0000000000000000000000000000000000000000000000000000000000000032"
+
+    const grantRole = await milkContract.connect(signer1).grantRole(DEPOSITOR_ROLE, signer2.address)
+    await grantRole.wait();
+    const balBefore = await milkContract.balanceOf(signer1.address);
+    const totalSupplyBefore = await milkContract.totalSupply();
+
+    const deposit = await milkContract.connect(signer2s).deposit(signer1.address, depositData);
+
+    const balanceAfter = await milkContract.balanceOf(signer1.address);
+    const totalSupplyAfter = await milkContract.totalSupply();
+  
+    const balance = Number(balBefore.toString()) + 50; 
+    const ts = Number(totalSupplyBefore.toString()) + 50;
+    expect(Number(balanceAfter.toString())).to.equal(balance);
+    expect(Number(totalSupplyAfter.toString())).to.equal(ts);
+  });
+
+  it("Withdraw Milk", async function () {
+    const [,signer, signer2] = await ethers.getSigners()
+    const signer1 = await ethers.getSigner(signer.address);
+    const signer2s = await ethers.getSigner(signer2.address);
+
+    const MASTER_ROLE = ethers.utils.id("MASTER_ROLE");
+
+    const grantRole = await milkContract.connect(signer1).grantRole(MASTER_ROLE, signer2.address)
+    await grantRole.wait();
+    const mint = await milkContract.connect(signer2s).mint(signer1.address, 200);
+    const balBefore = await milkContract.balanceOf(signer1.address);
+    const totalSupplyBefore = await milkContract.totalSupply();
+        
+    const burn = await milkContract.withdraw('100');
+    await burn.wait();
+
+    const balanceAfter = await milkContract.balanceOf(signer1.address);
+    const totalSupplyAfter = await milkContract.totalSupply();
     
-    // const signer1 = await ethers.getSigner(signer.address);
-    // const mint = await milkContract.mint(signer1.address, 200);
-    // const balBefore = await milkContract.balanceOf(signer1.address);
+    const balance = Number(balBefore.toString()) - 100; 
+    const ts = Number(totalSupplyBefore.toString()) - 100;
+    expect(Number(balanceAfter.toString())).to.equal(balance);
+    expect(Number(totalSupplyAfter.toString())).to.equal(ts);
+  });
 
-    // console.log(`The balance when mint is done ${balBefore}`);
+  
+  it("Mint Milk", async function () {
+    const [,signer, signer2] = await ethers.getSigners()
+    const signer1 = await ethers.getSigner(signer.address);
+    const signer2s = await ethers.getSigner(signer2.address);
+
+    const MASTER_ROLE = ethers.utils.id("MASTER_ROLE");
+
+    const grantRole = await milkContract.connect(signer1).grantRole(MASTER_ROLE, signer2.address)
+    await grantRole.wait();
+    const balBefore = await milkContract.balanceOf(signer1.address);
+    const totalSupplyBefore = await milkContract.totalSupply();
+    const mint = await milkContract.connect(signer2s).mint(signer1.address, 200);
+  
+
+    const balanceAfter = await milkContract.balanceOf(signer1.address);
+    const totalSupplyAfter = await milkContract.totalSupply();
     
-    // const burn = await milkContract.withdraw('100');
-
-    // const balanceOf = await milkContract.balanceOf(signer1.address);
-    // const balance = Number(balanceOf.toString()) - 100; 
-    // // const transferToContract = await ghostContractAddr.connect(signer1).transfer(ghostContractAddr.address, "9000000000000000000000");
-    // // const balContract = await ghostContractAddr.balanceOf(ghostContractAddr.address);
-    // expect(balanceOf.toString()).to.equal(balance);
+    const balance = Number(balBefore.toString()) + 200; 
+    const ts = Number(totalSupplyBefore.toString()) + 200;
+    expect(Number(balanceAfter.toString())).to.equal(balance);
+    expect(Number(totalSupplyAfter.toString())).to.equal(ts);
   });
 
 });
